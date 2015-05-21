93a1be8f2a9e99effbf4efd7d09a54491d67c66d
jgwin <jacobegwin@gmail.com>
2015-05-21 16:37:37 -0700

    Source files for room, door, maze, and a driver
    Some first draft hack and slash source files for how the door, room, and
    maze objects will look, as well as a driver which is used for loading a
    maze from an input file formatted with the rules laid out in the room
    input format for maze text file. There is also a tester I used basically
    just to make sure the maze was loading correctly and each room was being
    assigned the doors which it should have.

---
+++ src/MazeDriver.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+public class MazeDriver {
+	
+	private Maze maze;
+	
+	public MazeDriver()
+	{
+		maze = new Maze();
+	}
+	
+	public void createMaze(File inputfile)
+	{
+		int North,South,East,West,x,y;
+		Room[][] temp_Maze = null;
+		
+		try
+		{
+			Scanner fin = new Scanner(inputfile);
+			
+			x = fin.nextInt(); //get x dimention
+			y = fin.nextInt(); //get y dimention
+			fin.nextLine();    //clear buffer
+			
+			temp_Maze = new Room[x][y];
+			
+			for(int i = 0; i<x; i++)
+			{
+				for(int j = 0; j<y; j++)
+				{
+					String cur = fin.nextLine();
+					North = Character.getNumericValue(cur.charAt(0));
+					South = Character.getNumericValue(cur.charAt(1));
+					East = Character.getNumericValue(cur.charAt(2));
+					West = Character.getNumericValue(cur.charAt(3));
+					
+					Room temp = new Room(North,South,East,West);
+					
+					temp_Maze[i][j] = temp;
+				}
+			}
+			
+			this.maze = new Maze(temp_Maze,x,y);
+			fin.close();
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println("File was not found.");
+			System.err.println(inputfile.getAbsolutePath());
+		}
+	}
+	
+	public void printMaze()
+	{
+		System.out.println("maze is of dimention: "+maze.getX()+" "+maze.getY());
+
+		for(int i=0; i<maze.getX(); i++)
+		{
+			for(int j=0; j<maze.getY(); j++)
+			{
+				System.out.println("Room index: "+i+" "+j);
+				maze.getRoom(i,j).printRoom();
+				System.out.println();
+			}
+		}
+	}
+}
