3e49c96a33b00d30071362a2cdb7e65e0df97e12
Brady Clapp <bclapp@CSCD-CEB201W09.eastern.ewu.edu>
2015-06-10 11:43:49 -0700

    Refactoring and added functions

--- src/view/DoorElement.java
+++ src/view/DoorElement.java
@@ -1,33 +1,64 @@
-package view;
+//package view;
 
 import javax.swing.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class DoorElement
-{
+ public class DoorElement
+ {
   private JButton btn;
   private JLabel door;
+  private int in;
+ }
+ 
+ public int getIndex()
+ {
+  return in;
+ }
+  
+ public void setIndex(int index)
+ {
+  this.in = index;
+ }
+	
+ public JButton getBtn() 
+ {
+  return btn;
+ }
 
+ public void setBtn(JButton btn)
+ {	
+  this.btn = btn;
+ }
+ 
+ public JLabel getLbl() 
+ {
+  return door;
+ }
+ 
+ public void setLbl(JLabel door) 
+ {
+  this.door = door;
+ }
 
-  public DoorElement(JButton btn, JLabel door)
+ public DoorElement(JButton btn, JLabel door, int index)
+ {
+  this.btn = btn;
+  this.door = door;
+  this.in = index;
+ }
+
+ public void lockDoor()
+ {
+  try
   {
-    this.btn = btn;
-    this.door = door;
+   this.door.setIcon(new ImageIcon(new URL("http://i.imgur.com/9Iyt73S.jpg")));
+   this.btn.setText("Locked");
   }
-
-  public void lockDoor()
+  catch (MalformedURLException e)
   {
-    try
-    {
-      this.door.setIcon(new ImageIcon(new URL("//http://i.imgur.com/9Iyt73S.jpg")));
-      this.btn.setText("Locked");
-    }
-    catch (MalformedURLException e)
-    {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+   e.printStackTrace();
   }
+ }
 
 }
