f5c745362e8b9e2a12c6b6427a9ee59ba4969390
bclapp <bclapp2012@eagles.ewu.edu>
2015-06-10 21:12:34 -0700

    Added actions to multichoice

--- src/view/multiChoiceGUI.java
+++ src/view/multiChoiceGUI.java
@@ -27,48 +27,68 @@ public class multiChoiceGUI extends makeGUI
   public multiChoiceGUI(Question question)
   {
     this.question = question;
-
+    
+    //Database.checkAnswer("",question);
+    
     frame = new JFrame();
     frame.setTitle("Trivia");
     frame.setBounds(100, 100, 797, 425);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.getContentPane().setLayout(null);
 
-    JButton btnA = new JButton("A");
-    btnA.setForeground(Color.BLACK);
-    btnA.setBounds(101, 281, 146, 65);
-    frame.getContentPane().add(btnA);
+    
 
     JTextPane questionTxt = new JTextPane();
+    
+    //Set question text
     questionTxt.setBackground(SystemColor.menu);
     questionTxt.setText(question.getQuestion());
     questionTxt.setBounds(104, 96, 565, 84);
     frame.getContentPane().add(questionTxt);
-
+    
+    //A answer
     JTextPane aTxt = new JTextPane();
     aTxt.setText(question.getOptA());
     aTxt.setBounds(101, 225, 146, 45);
     frame.getContentPane().add(aTxt);
+    
+    //A button
+    JButton btnA = new JButton("A");
+    btnA.setForeground(Color.BLACK);
+    btnA.setBounds(101, 281, 146, 65);
+    frame.getContentPane().add(btnA);
+    
+    btnA.addActionListener(getAction(question.getOptA()));
 
+    
+    //B answer
     JTextPane bTxt = new JTextPane();
     bTxt.setText(question.getOptB());
     bTxt.setBounds(319, 225, 146, 45);
     frame.getContentPane().add(bTxt);
-
+    
+    
+    //B button
     JButton btnB = new JButton("B");
     btnB.setForeground(Color.BLACK);
     btnB.setBounds(319, 281, 146, 65);
     frame.getContentPane().add(btnB);
+    
+    btnB.addActionListener(getAction(question.getOptB()));
 
+    //C answer
     JTextPane cTxt = new JTextPane();
     cTxt.setText(question.getOptC());
     cTxt.setBounds(523, 225, 146, 45);
     frame.getContentPane().add(cTxt);
-
+    
+    //C button
     JButton btnC = new JButton("C");
     btnC.setForeground(Color.BLACK);
     btnC.setBounds(523, 281, 146, 65);
     frame.getContentPane().add(btnC);
+    
+    btnC.addActionListener(getAction(question.getOptC()));
 
 
   }
@@ -81,5 +101,55 @@ public void hide() {
 	frame.setVisible(false);
 	
 }
+
+//Action Listener Functions
+
+
+public ActionListener getAction(String text){
+	
+	if(Database.checkAnswer(text,question)){
+		return correctAnswer();
+	}
+	else
+		return wrongAnswer();
+	
+}
+
+
+
+public ActionListener correctAnswer()
+{
+	return new ActionListener(){
+		
+	public void actionPerformed(ActionEvent arg0) 
+	{
+		//Display dialog saying correct
+		JOptionPane.showMessageDialog(frame,
+			    "Correct answer!",
+			    "Congrats!",
+			    JOptionPane.PLAIN_MESSAGE);
+		setAnswer(1);
+		
+	}
+};
+}
+
+public ActionListener wrongAnswer()
+{
+	return new ActionListener(){
+		
+	public void actionPerformed(ActionEvent arg0) 
+	{
+		//Display dialog saying answer was wrong
+		JOptionPane.showMessageDialog(frame,
+			    "You got the answer wrong!",
+			    "Sorry!",
+			    JOptionPane.PLAIN_MESSAGE);
+		setAnswer(2);
+		
+	}
+};
+}
+
 }
 
